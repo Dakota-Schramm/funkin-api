3cd0b86404fe0fe317d0ca9a8f32d9db7aec2a47
Dakota Schramm <william.schramm11@gmail.com>
2022-07-24 14:44:24 -0400

    Change formatting.

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -7,17 +7,11 @@ verifyToken = (req, res, next) => {
   let token = req.headers['x-access-token']
 
   if (!token) {
-    return res.status(403).send({
-      message: 'No token provided!',
-    })
+    return res.status(403).send({ message: 'No token provided!' })
   }
 
   jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({
-        message: 'Unauthorized!',
-      })
-    }
+    if (err) return res.status(401).send({ message: 'Unauthorized!' })
 
     req.userId = decoded.id
     next()
@@ -34,9 +28,7 @@ isAdmin = (req, res, next) => {
         }
       }
 
-      res.status(403).send({
-        message: 'Require Admin Role!',
-      })
+      res.status(403).send({ message: 'Require Admin Role!' })
 
       return
     })
