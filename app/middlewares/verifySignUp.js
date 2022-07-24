74985d2faec69ea5064c40c9a5134df751fcdfde
Dakota Schramm <william.schramm11@gmail.com>
2022-07-24 15:00:01 -0400

    Formatting.

--- app/middlewares/verifySignUp.js
+++ app/middlewares/verifySignUp.js
@@ -5,9 +5,7 @@ const User = db.user
 checkDuplicateUsername = (req, res, next) => {
   // Username
   User.findOne({
-    where: {
-      username: req.body.username,
-    },
+    where: { username: req.body.username, }
   }).then(user => {
     if (user) {
       res.status(400).send({
