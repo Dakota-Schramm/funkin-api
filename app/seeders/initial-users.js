3c5de2ec1f51d611eeb2fc7a86c3398476977c4f
Dakota Schramm <william.schramm11@gmail.com>
2022-07-24 18:29:29 -0400

    Changed initial users seeder.

--- app/seeders/initial-users.js
+++ app/seeders/initial-users.js
@@ -8,18 +8,18 @@ var bcrypt = require('bcryptjs')
 const hashPassword = (password) => bcrypt.hashSync(password, 8)
 
 const MOCK_USERS = [
-  { userId: 0, username: 'boyfriend', password: hashPassword('idowhatiwant') },
-  { userId: 1, username: 'girlfriend', password: hashPassword('headempty') },
-  { userId: 2, username: 'daddy-dearest', password: hashPassword('mymearest') },
-  { userId: 3, username: 'mommy-mearest', password: hashPassword('demonPop') },
-  { userId: 4, username: 'skid', password: hashPassword('sp00ky') },
-  { userId: 5, username: 'pump', password: hashPassword('ITSSPOOKY') },
-  { userId: 6, username: 'monster', password: hashPassword('not-a-cannibal') },
-  { userId: 7, username: 'pico', password: hashPassword('gop1co') },
-  { userId: 8, username: 'senpai', password: hashPassword('rosesnthorns') },
-  { userId: 9, username: 'tankman', password: hashPassword('newgroundsveteran') },
-  { userId: 10, username: 'darnell', password: hashPassword('classthree') },
-  { userId: 11, username: 'nene', password: hashPassword('knife2meetu') }
+  { userId: 0, roleId: 1, username: 'boyfriend', password: hashPassword('idowhatiwant') },
+  { userId: 1, roleId: 0, username: 'girlfriend', password: hashPassword('headempty') },
+  { userId: 2, roleId: 0, username: 'daddy-dearest', password: hashPassword('mymearest') },
+  { userId: 3, roleId: 0, username: 'mommy-mearest', password: hashPassword('demonPop') },
+  { userId: 4, roleId: 0, username: 'skid', password: hashPassword('sp00ky') },
+  { userId: 5, roleId: 0, username: 'pump', password: hashPassword('ITSSPOOKY') },
+  { userId: 6, roleId: 0, username: 'monster', password: hashPassword('not-a-cannibal') },
+  { userId: 7, roleId: 0, username: 'pico', password: hashPassword('gop1co') },
+  { userId: 8, roleId: 0, username: 'senpai', password: hashPassword('rosesnthorns') },
+  { userId: 9, roleId: 0, username: 'tankman', password: hashPassword('newgroundsveteran') },
+  { userId: 10, roleId: 0, username: 'darnell', password: hashPassword('classthree') },
+  { userId: 11, roleId: 0, username: 'nene', password: hashPassword('knife2meetu') }
 ]
 
 module.exports = {
