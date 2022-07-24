c670a35584548b41f78f1b5b0bd9149722f506d4
Dakota Schramm <william.schramm11@gmail.com>
2022-07-24 13:37:13 -0400

    Added controller for extra database queries. Started seeders.

---
+++ app/seeders/initial-song.js
@@ -0,0 +1,72 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Songs', [
+      {
+        songname: 'bopeebo'
+      },
+      {
+        songname: 'fresh'
+      },
+      {
+        songname: 'dadbattle'
+      },
+      {
+        songname: 'spookeez'
+      },
+      {
+        songname: 'south'
+      },
+      {
+        songname: 'monster'
+      },
+      {
+        songname: 'pico'
+      },
+      {
+        songname: 'philly'
+      },
+      {
+        songname: 'blammed'
+      },
+      {
+        songname: 'satin-panties'
+      },
+      {
+        songname: 'high'
+      },
+      {
+        songname: 'milf'
+      },
+      {
+        songname: 'cocoa'
+      },
+      {
+        songname: 'eggnog'
+      },
+      {
+        songname: 'winter-horrorland'
+      },
+      {
+        songname: 'senpai'
+      },
+      {
+        songname: 'roses'
+      },
+      {
+        songname: 'thorns'
+      },
+      {
+        songname: 'ugh'
+      },
+      {
+        songname: 'stress'
+      },
+      {
+        songname: 'guns'
+      },
+    ])
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Songs', null, {})
+  },
+}
