39c144d281c27984a62210f6156b89790e51c874
Dakota Schramm <william.schramm11@gmail.com>
2022-07-24 22:20:27 -0400

    Fixed query routes to properly pull from database.

--- app/controllers/query.controller.js
+++ app/controllers/query.controller.js
@@ -6,22 +6,31 @@ const Song = db.song
 const Op = db.Sequelize.Op
 
 exports.allScores = async (req, res) => {
-  const scores = await Score.findAll()
+  const scores = await Score.findAll({
+    attributes: ['score', 'userId', 'scoreId']
+  })
   res.status(200).send(scores)
 }
 
 exports.allUsers = async (req, res) => {
-  const users = await User.findAll()
+  const users = await User.findAll({
+    attributes: ['userId', 'roleId', 'username', 'password']
+  })
   res.status(200).send(users)
 }
 
 exports.allSongs = async (req, res) => {
-  const songs = await Song.findAll();
+  const songs = await Song.findAll({
+    attributes: ['songname', 'songId']
+  });
   res.status(200).send(songs);
 }
 
 exports.scoresFromUsers = (req, res) => {
-  Score.findAll({ where: { userId: req.params.users} })
+  Score.findAll({ 
+    where: { userId: req.params.userid} ,
+    attributes: ['score', 'userId', 'scoreId']
+  })
   .then(scores => res.status(200).send(scores))
   .catch(err => res.status(500).send({ message: err.message }) )
 }
