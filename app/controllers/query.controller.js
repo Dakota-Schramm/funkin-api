e8498f735bcb59861ea1e8ea87c05caf2064450d
Dakota Schramm <william.schramm11@gmail.com>
2022-07-24 21:55:56 -0400

    Implemented query controller.

--- app/controllers/query.controller.js
+++ app/controllers/query.controller.js
@@ -1,86 +1,27 @@
 const db = require('../models')
-const config = require('../config/auth.config')
 const User = db.user
+const Role = db.role
+const Score = db.score
+const Song = db.song
 const Op = db.Sequelize.Op
 
-var jwt = require('jsonwebtoken')
-var bcrypt = require('bcryptjs')
-
-exports.signup = (req, res) => {
-  // create new User in database (role is user if not specifying role)
-  // Save User to Database
-  User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8),
-  })
-    .then(user => {
-      if (req.body.roles) {
-        Role.findAll({
-          where: {
-            name: {
-              [Op.or]: req.body.roles,
-            },
-          },
-        }).then(roles => {
-          user.setRoles(roles).then(() => {
-            res.send({ message: 'User was registered successfully!' })
-          })
-        })
-      } else {
-        // user role = 1
-        user.setRoles([1]).then(() => {
-          res.send({ message: 'User was registered successfully!' })
-        })
-      }
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message })
-    })
+exports.allScores = async (req, res) => {
+  const scores = await Score.findAll()
+  res.status(200).send(scores)
 }
 
-exports.signin = (req, res) => {
-  /*
-    find username of the request in database, if it exists
-    compare password with password in database using bcrypt, if it is correct
-    generate a token using jsonwebtoken
-    return user information & access Token
-  */
-  User.findOne({
-    where: {
-      username: req.body.username,
-    },
-  })
-    .then(user => {
-      if (!user) return res.status(404).send({ message: 'User Not found.' })
+exports.allUsers = async (req, res) => {
+  const users = await User.findAll()
+  res.status(200).send(users)
+}
 
-      var passwordIsValid = bcrypt.compareSync(req.body.password, user.password)
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: 'Invalid Password!',
-        })
-      }
+exports.allSongs = async (req, res) => {
+  const songs = await Song.findAll();
+  res.status(200).send(songs);
+}
 
-      var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400, // 24 hours
-      })
-
-      var authorities = []
-      user.getRoles().then(roles => {
-        for (let i = 0; i < roles.length; i++) {
-          authorities.push('ROLE_' + roles[i].name.toUpperCase())
-        }
-        res.status(200).send({
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          roles: authorities,
-          accessToken: token,
-        })
-      })
-    })
-    .catch(err => {
-      res.status(500).send({ message: err.message })
-    })
+exports.scoresFromUsers = (req, res) => {
+  Score.findAll({ where: { userId: req.params.users} })
+  .then(scores => res.status(200).send(scores))
+  .catch(err => res.status(500).send({ message: err.message }) )
 }
